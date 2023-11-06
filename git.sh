8606cd543c334e794245051ba825ccc19cd90f6d
moustapndiaye <ndiayetapha7@gmail.com>
2023-11-06 20:22:52 +0000

    m

---
+++ git.sh
@@ -0,0 +1,6 @@
+git config --global credential.helper store
+git clone https://learn.zone01dakar.sn/git/moustapndiaye/make-your-game.git
+cd make-your-game
+git config --global user.email "ndiayetapha7@gmail.com"   
+git config --global user.name "moustapndiaye"  
+code -r .
