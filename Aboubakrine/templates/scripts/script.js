8a689b001d41ab8e0d9e3101f36b8c0fa950777a
aboubakdiallo <babacardiallo2121@gmail.com>
2023-11-18 19:13:07 +0000

    export of the pattern

---
+++ Aboubakrine/templates/scripts/script.js
@@ -0,0 +1,43 @@
+// import { pattern } from "./pattern,js";
+
+const gridSize = 15;
+const grid = document.getElementById('grid');
+const cells = [];
+
+const pattern = [
+    "XXXXXXXXXXXXXXX",
+    "XVVVVVVVVVVVVVX",
+    "XVXVXVXVXVXVXVX",
+    "XVVVVVVVVVVVVVX",
+    "XVXVXVXVXVXVXVX",
+    "XVVVVVVVVVVVVVX",
+    "XVXVXVXVXVXVXVX",
+    "XVVVVVVVVVVVVVX",
+    "XVXVXVXVXVXVXVX",
+    "XVVVVVVVVVVVVVX",
+    "XVXVXVXVXVXVXVX",
+    "XVVVVVVVVVVVVVX",
+    "XVXVXVXVXVXVXVX",
+    "XVVVVVVVVVVVVVX",
+    "XXXXXXXXXXXXXXX"
+];
+
+
+// Initialisation grid de base
+for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+        const cell = document.createElement('div');
+        cell.dataset.row = i;
+        cell.dataset.col = j;
+
+        // placement murs et cellules vides
+        if (pattern[i][j] === "X") {
+            cell.className = 'cell wall';
+        } else {
+            cell.className = 'cell empty';
+        }
+
+        cells.push(cell);
+        grid.appendChild(cell);
+    }
+}
