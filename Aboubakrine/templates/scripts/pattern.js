7d5c5aefaf89f795a1301d8b73ea2f9ad17eb1bd
aboubakdiallo <babacardiallo2121@gmail.com>
2023-11-21 13:10:47 +0000

    mis en 2d du pattern

--- Aboubakrine/templates/scripts/pattern.js
+++ Aboubakrine/templates/scripts/pattern.js
@@ -1,19 +1,19 @@
 export const pattern5 = [
-    "XXXXXXXXXXXXXXX",
-    "XVVVVVBBBVVBBVX",
-    "XVXVXVXBXVXVXVX",
-    "XBVVVVVVVVVVBBX",
-    "XBXVXVXBXVXVXVX",
-    "XVVVVVVBVVVVVVX",
-    "XVXVXVXBXVXVXVX",
-    "XVVVVVBVVVVVVBX",
-    "XVXVXVXVXBXVXVX",
-    "XVVVVVVBBVVVVVX",
-    "XVXVXVXVXVXVXVX",
-    "XVVVVVVBBVVVVVX",
-    "XBXBXVXVXVXVXVX",
-    "XVVVVVVVVBVVVVX",
-    "XXXXXXXXXXXXXXX"
+    ["X","X","X","X","X","X","X","X","X","X","X","X","X","X","X"],
+    ["X",'V','V','V','V','V','B','B','B','V','V','B','B','V',"X"],
+    ["X","V","X","V","X","V","X","B","X","V","X","V","X","V","X"],
+    ['X','B','V','V','V','V','V','V','V','V','V','V','B','B','X'],
+    ['X','B','X','V','X','V','X','B','X','V','X','V','X','V','X'],
+    ['X','V','V','V','V','V','V','B','V','V','V','V','V','V','X'],
+    ['X','V','X','V','X','V','X','B','X','V','X','V','X','V','X'],
+    ['X','V','V','V','V','V','B','V','V','V','V','V','V','B','X'],
+    ['X','V','X','V','X','V','X','V','X','B','X','V','X','V','X'],
+    ['X','V','V','V','V','V','V','B','B','V','V','V','V','V','X'],
+    ['X','V','X','V','X','V','X','V','X','V','X','V','X','V','X'],
+    ['X','V','V','V','V','V','V','B','B','V','V','V','V','V','X'],
+    ['X','B','X','B','X','V','X','V','X','V','X','V','X','V','X'],
+    ['X','V','V','V','V','V','V','V','V','B','V','V','V','V','X'],
+    ["X","X","X","X","X","X","X","X","X","X","X","X","X","X","X"]
 ];
 
 export const pattern1 = [
