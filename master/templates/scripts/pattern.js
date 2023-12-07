9b75dca3c152602df00098dcacf59ffc87b35d74
aboubakdiallo <babacardiallo2121@gmail.com>
2023-12-07 18:32:22 +0000

    merge to master directory

---
+++ master/templates/scripts/pattern.js
@@ -0,0 +1,106 @@
+export const pattern5 = [
+    ["X","X","X","X","X","X","X","X","X","X","X","X","X","X","X"],
+    ["X",'V','V','V','V','V','B','B','B','V','V','B','B','V',"X"],
+    ["X","V","X","V","X","V","X","B","X","V","X","V","X","V","X"],
+    ['X','B','V','V','V','V','V','V','V','V','V','V','B','B','X'],
+    ['X','B','X','V','X','V','X','B','X','V','X','V','X','V','X'],
+    ['X','V','V','V','V','V','V','B','V','V','V','V','V','V','X'],
+    ['X','V','X','V','X','V','X','B','X','V','X','V','X','V','X'],
+    ['X','V','V','V','V','V','B','V','V','V','V','V','V','B','X'],
+    ['X','V','X','V','X','V','X','V','X','B','X','V','X','V','X'],
+    ['X','V','V','V','V','V','V','B','B','V','V','V','V','V','X'],
+    ['X','V','X','V','X','V','X','V','X','V','X','V','X','V','X'],
+    ['X','V','V','V','V','V','V','B','B','V','V','V','V','V','X'],
+    ['X','B','X','B','X','V','X','V','X','V','X','V','X','V','X'],
+    ['X','V','V','V','V','V','V','V','V','B','V','V','V','V','X'],
+    ["X","X","X","X","X","X","X","X","X","X","X","X","X","X","X"]
+];
+
+export const pattern1 = [
+    "XXXXXXXXXXXXXXX",
+    "XVVVVVVVVVVVVVX",
+    "XVXVXVXBXVXVXVX",
+    "XBVVVVVVVVVVBBX",
+    "XBXVXVXBXVXVXVX",
+    "XVVVVVVVVVVVVVX",
+    "XVXVXVXBXVXVXVX",
+    "XBVVVVVBVVVVVBX",
+    "XVXVXVXVXBXVXVX",
+    "XVVVVVVBBVVVVVX",
+    "XVXVXVXVXVXVXVX",
+    "XVVVVVVBBVVVVVX",
+    "XBXBXVXVXVXVXVX",
+    "XVVVVVVVVBVVVVX",
+    "XXXXXXXXXXXXXXX"
+];
+export const pattern2 = [
+    "XXXXXXXXXXXXXXX",
+    "XVVVVVBBBVVBBVX",
+    "XVXVXVXBXVXVXVX",
+    "XBVVVVVVVVVVBBX",
+    "XBXVXVXBXVXVXVX",
+    "XVVVVVVBVVVVVVX",
+    "XVXVXVXBXVXVXVX",
+    "XVVVVVBVVVVVVBX",
+    "XVXVXVXVXBXVXVX",
+    "XVVVVVVBBVVVVVX",
+    "XVXVXVXVXVXVXVX",
+    "XVVVVVVBBVVVVVX",
+    "XBXBXVXVXVXVXVX",
+    "XVVVVVVVVBVVVVX",
+    "XXXXXXXXXXXXXXX"
+];
+
+export const pattern3 = [
+    "XXXXXXXXXXXXXXX",
+    "XVVVVVBBBVVBBVX",
+    "XVXVXVXBXVXVXVX",
+    "XBVVVVVVVVVVBBX",
+    "XBXVXVXBXVXVXVX",
+    "XVVVVVVBVVVVVVX",
+    "XVXVXVXBXVXVXVX",
+    "XBVVVVVBVVVVVBX",
+    "XVXVXVXVXBXVXVX",
+    "XVVVVVVBBVVVVVX",
+    "XVXVXVXVXVXVXVX",
+    "XVVVVVVBBVVVVVX",
+    "XBXBXVXVXVXVXVX",
+    "XVVVVVVVVBVVVVX",
+    "XXXXXXXXXXXXXXX"
+];
+
+export const pattern4 = [
+    "XXXXXXXXXXXXXXX",
+    "XVVVVVBBBVVBBVX",
+    "XVXVXVXBXVXVXVX",
+    "XBVVVVVVVVVVBBX",
+    "XBXVXVXBXVXVXVX",
+    "XVVVVVVBVVVVVVX",
+    "XVXVXVXBXVXVXVX",
+    "XVVVVVBVVVVVVBX",
+    "XVXVXVXVXBXVXVX",
+    "XVVVVVVBBVVVVVX",
+    "XVXVXVXVXVXVXVX",
+    "XVVVVVVBBVVVVVX",
+    "XBXBXVXVXVXVXVX",
+    "XVVVVVVVVBVVVVX",
+    "XXXXXXXXXXXXXXX"
+];
+
+export const pattern = [
+    "XXXXXXXXXXXXXXX",
+    "XVVVVVBBBVVBBVX",
+    "XVXVXVXBXVXVXVX",
+    "XBVVVVVVVVVVBBX",
+    "XBXVXVXBXVXVXVX",
+    "XVVVVVBVVVVVVVX",
+    "XVXVXVXBXVXVXVX",
+    "XBVVVVVBVVVVVBX",
+    "XVXVXVXVXBXVXVX",
+    "XVVVVVVBBVVVVVX",
+    "XVXVXVXVXVXVXVX",
+    "XVVVVVVBBVVVVVX",
+    "XBXBXVXVXVXVXVX",
+    "XVVVVVVVVBVVVVX",
+    "XXXXXXXXXXXXXXX"
+];
