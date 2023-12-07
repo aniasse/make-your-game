6a3b16d8d160af7a2d589f2876ae759560af054e
moustapndiaye <ndiayetapha7@gmail.com>
2023-12-07 19:01:00 +0000

    m

--- master/templates/scripts/script.js
+++ master/templates/scripts/script.js
@@ -21,7 +21,7 @@ for (let i = 0; i < gridSize; i++) {
         if (i === 1 && j === 1) {
             cell.appendChild(playerDiv);
         }
-
+        
         // placement murs player et cellules vides
         if (model[i][j] === "X") {
             cell.className = 'cell wall';
@@ -30,7 +30,7 @@ for (let i = 0; i < gridSize; i++) {
         } else {
             cell.className = 'cell empty';
         }
-
+        
         cells.push(cell);
         grid.appendChild(cell);
     }
@@ -43,154 +43,157 @@ for (let i = 0; i < gridSize; i++) {
 document.addEventListener('keydown', (event) => {
     if (!pausemenu) {
         const playerPos = playerDiv;
-
+        
         let newRow = parseInt(playerDiv.dataset.row);
         let newCol = parseInt(playerDiv.dataset.col);
-
+        
         switch (event.key) {
             case 'ArrowUp':
-                playerDiv.style.backgroundImage = `url(${path}back-${leg === 'right' ? '1' : '2'}.png)`;
-                leg = leg === 'right' ? 'left' : 'right';
-                newRow--;
-                break;
+            playerDiv.style.backgroundImage = `url(${path}back-${leg === 'right' ? '1' : '2'}.png)`;
+            leg = leg === 'right' ? 'left' : 'right';
+            newRow--;
+            break;
             case 'ArrowDown':
-                playerDiv.style.backgroundImage = `url(${path}front-${leg === 'right' ? '1' : '2'}.png)`;
-                leg = leg === 'right' ? 'left' : 'right';
-                newRow++;
-                break;
+            playerDiv.style.backgroundImage = `url(${path}front-${leg === 'right' ? '1' : '2'}.png)`;
+            leg = leg === 'right' ? 'left' : 'right';
+            newRow++;
+            break;
             case 'ArrowLeft':
-                playerDiv.style.backgroundImage = `url(${path}left-${leg === 'right' ? '1' : '2'}.png)`;
-                leg = leg === 'right' ? 'left' : 'right';
-                newCol--;
-                break;
+            playerDiv.style.backgroundImage = `url(${path}left-${leg === 'right' ? '1' : '2'}.png)`;
+            leg = leg === 'right' ? 'left' : 'right';
+            newCol--;
+            break;
             case 'ArrowRight':
-                playerDiv.style.backgroundImage = `url(${path}right-${leg === 'right' ? '1' : '2'}.png)`;
-                leg = leg === 'right' ? 'left' : 'right';
-                newCol++;
-                break;
+            playerDiv.style.backgroundImage = `url(${path}right-${leg === 'right' ? '1' : '2'}.png)`;
+            leg = leg === 'right' ? 'left' : 'right';
+            newCol++;
+            break;
             case ' ':
             case 'Spacebar':
-                placeBomb();
-                console.log('placement bomb');
-                return;
+            placeBomb();
+            console.log('placement bomb');
+            return;
             default:
-                return;
+            return;
         }
-
+        
         // Vérifiez si les nouvelles coordonnées sont dans les limites de la grille
         if (
             newRow >= 0 && newRow < gridSize &&
             newCol >= 0 && newCol < gridSize &&
             model[newRow][newCol] !== "X" &&
             model[newRow][newCol] !== "B"
-        ) {
-            playerDiv.style.transition = "top 0.2s ease, left 0.2s ease";
-            playerDiv.dataset.row = newRow;
-            playerDiv.dataset.col = newCol;
-            playerDiv.style.top = `${newRow * 40}px`;
-            playerDiv.style.left = `${newCol * 40}px`;
-        }
-    }
-}); 
-
-/* placement bomb */
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function placeBomb() {
-    const bombPos = { row: parseInt(playerDiv.dataset.row), col: parseInt(playerDiv.dataset.col) };
-    const bombCell = cells[bombPos.row * gridSize + bombPos.col];
-
-    bombCell.classList.add('bomb');
-
-    await delay(bombDelay);
-    explodeBomb(bombPos);
-}
-
-async function explodeBomb(bombPos) {
-    const bombCell = cells[bombPos.row * gridSize + bombPos.col];
-    bombCell.classList.remove('bomb');
-
-    propagateExplosion(bombPos.row, bombPos.col);
-    await delay(100);
-    propagateExplosion(bombPos.row - 1, bombPos.col);
-    await delay(100);
-    propagateExplosion(bombPos.row + 1, bombPos.col);
-    await delay(100);
-    propagateExplosion(bombPos.row, bombPos.col - 1);
-    await delay(100);
-    propagateExplosion(bombPos.row, bombPos.col + 1);
-}
-
-// logique d'explosion
-function propagateExplosion(row, col) {
-    const currentCell = cells[row * gridSize + col];
-    if (model[row][col] !== 'X') {
-        currentCell.classList.add('onde');
-    }
-
-    setTimeout(() => {
-        currentCell.classList.remove('onde');
-        if (currentCell.classList.contains('player')) {
-            lives--;
-            console.log(lives);
-            console.log('you lose One live point');
-
-            if (lives === 0) {
-                console.log('Game Over');
+            ) {
+                playerDiv.style.transition = "top 0.2s ease, left 0.2s ease";
+                playerDiv.dataset.row = newRow;
+                playerDiv.dataset.col = newCol;
+                playerDiv.style.top = `${newRow * 40}px`;
+                playerDiv.style.left = `${newCol * 40}px`;
             }
         }
-    }, 1000);
-
-    // destruction brick
-    if (model[row][col] === 'B') {
-        model[row][col] = 'V';
+    }); 
+    
+    /* placement bomb */
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
-
-    if (model[row][col] === 'V') {
-        currentCell.classList.remove('brick');
-        currentCell.classList.add('empty');
+    
+    async function placeBomb() {
+        const bombPos = { row: parseInt(playerDiv.dataset.row), col: parseInt(playerDiv.dataset.col) };
+        const bombCell = cells[bombPos.row * gridSize + bombPos.col];
+        
+        bombCell.classList.add('bomb');
+        
+        await delay(bombDelay);
+        explodeBomb(bombPos);
     }
-}
-
-
-
-/// pause
-
-function work() {
-    function ResumeGame() {
-        alert("RESUME")
+    
+    async function explodeBomb(bombPos) {
+        const bombCell = cells[bombPos.row * gridSize + bombPos.col];
+        bombCell.classList.remove('bomb');
+        
+        propagateExplosion(bombPos.row, bombPos.col);
+        await delay(100);
+        propagateExplosion(bombPos.row - 1, bombPos.col);
+        await delay(100);
+        propagateExplosion(bombPos.row + 1, bombPos.col);
+        await delay(100);
+        propagateExplosion(bombPos.row, bombPos.col - 1);
+        await delay(100);
+        propagateExplosion(bombPos.row, bombPos.col + 1);
     }
-    function RestartGame() {
-        alert("RESTART")
-    }
-    function ExitFame() {
-        alert("QUIT GAME")
-    }
-    const CONTINUE = document.getElementById("CONTINUE")
-    const RESTART = document.getElementById("RESTART")
-    const EXIT = document.getElementById("EXIT")
-    CONTINUE.addEventListener("click", ResumeGame)
-    RESTART.addEventListener("click", RestartGame)
-    EXIT.addEventListener("click", ExitFame)
-}
-
-document.addEventListener('DOMContentLoaded', work)
-
-document.addEventListener(
-    'keydown',
-    (event) => {
-        if (event.key == 'Escape') {
-            if (pausemenu) {
-                document.getElementById("pauseContainer").style.zIndex = "0";
-                pausemenu = false
-                document.body.classList.remove("menu-overlay");
-
-            } else {
-                document.getElementById("pauseContainer").style.zIndex = "2";
-                pausemenu = true
-                document.body.classList.add("menu-overlay");
-            }
+    
+    // logique d'explosion
+    function propagateExplosion(row, col) {
+        const currentCell = cells[row * gridSize + col];
+        if (model[row][col] !== 'X') {
+            currentCell.classList.add('onde');
         }
-    })
+        
+        setTimeout(() => {
+            currentCell.classList.remove('onde');
+            if (currentCell.classList.contains('player')) {
+                lives--;
+                console.log(lives);
+                console.log('you lose One live point');
+                
+                if (lives === 0) {
+                    console.log('Game Over');
+                }
+            }
+        }, 1000);
+        
+        // destruction brick
+        if (model[row][col] === 'B') {
+            model[row][col] = 'V';
+        }
+        
+        if (model[row][col] === 'V') {
+            currentCell.classList.remove('brick');
+            currentCell.classList.add('empty');
+        }
+    }
+    
+    
+    
+    /// pause
+    
+    function work() {
+        function ResumeGame() {
+            document.getElementById("pauseContainer").style.zIndex = "0";
+            pausemenu = false
+            document.body.classList.remove("menu-overlay");
+        }
+        function RestartGame() {
+            alert("RESTART")
+        }
+        function ExitFame() {
+            alert("QUIT GAME")
+        }
+        const CONTINUE = document.getElementById("CONTINUE")
+        const RESTART = document.getElementById("RESTART")
+        const EXIT = document.getElementById("EXIT")
+        CONTINUE.addEventListener("click", ResumeGame)
+        RESTART.addEventListener("click", RestartGame)
+        EXIT.addEventListener("click", ExitFame)
+    }
+    
+    document.addEventListener('DOMContentLoaded', work)
+    
+    document.addEventListener(
+        'keydown',
+        (event) => {
+            if (event.key == 'Escape') {
+                if (pausemenu) {
+                    document.getElementById("pauseContainer").style.zIndex = "0";
+                    pausemenu = false
+                    document.body.classList.remove("menu-overlay");
+                    
+                } else {
+                    document.getElementById("pauseContainer").style.zIndex = "2";
+                    pausemenu = true
+                    document.body.classList.add("menu-overlay");
+                }
+            }
+        })
+        
